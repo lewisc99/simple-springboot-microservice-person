5d4bf85e0e3ffa689cdba47f2597f4f8b6fe170d
lewisc99 <lewiscontato99@gmail.com>
2022-12-26 09:45:08 -0300

    Initial commit

---
+++ user/src/main/java/com/lewis/user/models/User.java
@@ -0,0 +1,39 @@
+package com.lewis.user.models;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String doc;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDoc() {
+        return doc;
+    }
+
+    public void setDoc(String doc) {
+        this.doc = doc;
+    }
+}
