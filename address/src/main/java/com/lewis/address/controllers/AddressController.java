7cf550f95bc584639b1f4a29146c7a6f7e5146b1
lewisc99 <lewiscontato99@gmail.com>
2022-12-27 10:48:14 -0300

    seed h2-database in address microservice

--- address/src/main/java/com/lewis/address/controllers/AddressController.java
+++ address/src/main/java/com/lewis/address/controllers/AddressController.java
@@ -1,13 +1,12 @@
 package com.lewis.address.controllers;
 
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/address")
 public class AddressController {
-    
+
+
 
 }
---
+++ address/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_address (city,country,name)  VALUES ('Vespasiano','Brazil','lewis');
+INSERT INTO tb_address (city,country,name)  VALUES ('Belo Horizonte','Brazil','Felipe');
+INSERT INTO tb_address (city,country,name)  VALUES ('Boston','United States','Rickson');
+
+
